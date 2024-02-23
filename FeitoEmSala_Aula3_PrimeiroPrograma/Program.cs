3111483a04641a02b8c49834a4362e8c286ec6d4
KaBrz23 <kauefernandesbraz@gmail.com>
2024-02-22 22:59:33 -0300

    Primeiro programa

---
+++ FeitoEmSala_Aula3_PrimeiroPrograma/Program.cs
@@ -0,0 +1,65 @@
+﻿Console.WriteLine("Bem vindo à nossa calculadora da FIAP - Turma 2TDSPX");
+
+var controleExecucao = true;
+var listaExecucoes = new List<String>();
+while (controleExecucao)
+{
+
+    Console.WriteLine();
+    Console.WriteLine("Digite 1 para: Digitar dois números para retornar a soma entre os dois");
+    Console.WriteLine("Digite 2 para: Digitar dois números para retornar a subtração entre os dois");
+    Console.WriteLine("Digite 5 para: Retornar todas as operações realizadas");
+    Console.WriteLine("Digite 6 para: Sair do programa");
+
+    var opcaoEscolhida = int.Parse(Console.ReadLine());
+
+//Console.WriteLine("A opção escolhida foi: " + opcaoEscolhida);
+
+    switch (opcaoEscolhida)
+    {
+        case 1:
+            Console.WriteLine("Digite o primeiro número: ");
+            double primeiroNumeroSoma = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número: ");
+            double segundoNumeroSoma = double.Parse(Console.ReadLine());
+
+            var resultado = primeiroNumeroSoma + segundoNumeroSoma;
+
+            //Console.WriteLine("O resultado é:" + resultado + ". Você gostou dessa forma na opção" + opcaoEscolhida);
+            var mensagem1 = $"O resultado é {resultado}. Você gostou dessa forma na opção {opcaoEscolhida}?";
+            Console.WriteLine(mensagem1);
+            listaExecucoes.Add(mensagem1);
+            //Console.WriteLine(string.Format("O resultado é {0}. Você gostou dessa forma na opção {1}", resultado, opcaoEscolhida));
+            break;
+        case 2:
+            Console.WriteLine("Digite o primeiro número: ");
+            double primeiroNumeroSub = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número: ");
+            double segundoNumeroSub = double.Parse(Console.ReadLine());
+
+            var resultadoSub = primeiroNumeroSub - segundoNumeroSub;
+
+            //Console.WriteLine("O resultado é:" + resultado + ". Você gostou dessa forma na opção" + opcaoEscolhida);
+            var mensagem2 = $"O resultado é {resultadoSub}. Você gostou dessa forma na opção {opcaoEscolhida}?";
+            Console.WriteLine(mensagem2);
+            listaExecucoes.Add(mensagem2);
+            break;
+        case 5:
+            Console.WriteLine();
+            Console.WriteLine("A lista de tarefas executadas é:");
+            Console.WriteLine();
+
+            foreach (var execucao in listaExecucoes)
+            {
+                Console.WriteLine($"Executado em: {DateTime.Now.ToString("dd/MM/yyyy")}: {execucao}");
+            }
+            break;
+        case 6:
+            controleExecucao = false;
+            break;
+        default:
+            break;
+    }
+}
---
+++ FeitoEmSala_Aula_3_InstrucoesBasicas/Program.cs
@@ -0,0 +1,80 @@
+﻿//instruções básicas
+
+//Declaração de Variáveis
+int idade =10;
+string nome;
+string descricao = "";
+var endereco = "";
+
+nome = "nome teste";
+
+//controles de fluxo
+if (idade > 15)
+{
+    Console.WriteLine(nome);
+    Console.WriteLine(descricao);
+}
+
+if (idade < 10)
+{
+    Console.WriteLine(nome);
+}
+else if (idade > 15)
+{
+    
+}
+else
+{
+    
+}
+
+var mesAno = 1;
+
+switch (mesAno)
+{
+    case 1:
+        Console.WriteLine("Tem o feriado do Ano Novo");
+        break;
+    case 4:
+        Console.WriteLine("Tem o feriado de Tiradentes");
+        break;
+    default:
+        break;
+}
+
+//Laços de repetição
+
+var contador = 1;
+while (contador <10)
+{
+    Console.WriteLine($"contador: {contador}");
+    contador++;
+}
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+}
+
+var listaNomes = new List<String>();
+listaNomes.Add("Nome 1");
+listaNomes.Add("Nome 2");
+listaNomes.Add("Nome 3");
+foreach (var s in listaNomes)
+{
+    Console.WriteLine(s);
+}
+
+//////////////////////////////////////////////
+ExibirMensagem();
+ExibirMensagemTexto("Teste parâmetro");
+void ExibirMensagem()
+{
+    Console.WriteLine("Testando um método");
+}
+
+void ExibirMensagemTexto(string texto)
+{
+    Console.WriteLine(texto);
+}
+
