39158ac916d6bf4fb0df69d702ff2e4981dc83f2
KaBrz23 <kauefernandesbraz@gmail.com>
2024-02-22 23:11:40 -0300

    primeiro programa

--- FeitoEmSala_Aula3_PrimeiroPrograma/obj/Debug/net8.0/FeitoEmSala_Aula3_PrimeiroPrograma.AssemblyInfo.cs
+++ FeitoEmSala_Aula3_PrimeiroPrograma/obj/Debug/net8.0/FeitoEmSala_Aula3_PrimeiroPrograma.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FeitoEmSala_Aula3_PrimeiroPrograma")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3111483a04641a02b8c49834a4362e8c286ec6d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("FeitoEmSala_Aula3_PrimeiroPrograma")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FeitoEmSala_Aula3_PrimeiroPrograma")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
